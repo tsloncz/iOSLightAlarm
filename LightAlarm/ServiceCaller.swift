237a8dee6e66a51f38072323c38c404a183b17fe
Tim Sloncz <tsloncz@gmail.com>
2016-01-24 01:00:53 -0600

    base app

---
+++ LightAlarm/ServiceCaller.swift
@@ -0,0 +1,85 @@
+//
+//  ServiceCaller.swift
+//  LightAlarm
+//
+//  Created by Tim Sloncz on 1/24/16.
+//  Copyright © 2016 Tim Sloncz. All rights reserved.
+//
+
+//
+//  RESTCallManager.swift
+//  DFW Trails
+//
+//  Created by Tim Sloncz on 12/23/15.
+//  Copyright © 2015 Tim Sloncz. All rights reserved.
+//
+
+import Foundation
+
+protocol ServiceCallProtocol {
+    func updateData(data: NSArray)
+}
+
+class ServiceCaller {
+    var delegate: ServiceCallProtocol!
+    
+    init(invoker: ServiceCallProtocol) {
+        self.delegate = invoker
+    }
+    
+    var responseCompletionHandler = {
+        print("response")
+    }
+    var response: NSArray!
+    
+    func makeCall(uri: String) -> NSArray {
+        
+        if let url = NSURL(string: uri) {
+            
+            let urlRequest = NSURLRequest(URL: url)
+            
+            let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+            let session = NSURLSession(configuration: config)
+            
+            let task = session.dataTaskWithRequest(urlRequest, completionHandler: {
+                (data, response, error) in
+                guard let responseData = data else {
+                    print("Error: did not receive data")
+                    return
+                }
+                guard error == nil else {
+                    print("error calling GET on /posts/1")
+                    print(error)
+                    return
+                }
+                // parse the result as JSON, since that's what the API provides
+                var post: NSArray
+                do {
+                    post = try NSJSONSerialization.JSONObjectWithData(responseData,
+                        options: []) as! NSArray
+                    self.response = post
+                    
+                } catch  {
+                    print("error trying to convert data to JSON")
+                    return
+                }
+                
+                // Tell invoker that we got data so update
+                self.delegate.updateData(self.response)
+            })
+            task.resume()
+        } else {
+            print("Error: cannot create URL")
+        }
+        
+        if response != nil {
+            return response
+        } else {
+            return []
+        }
+    }
+    
+    func getJson() -> NSDictionary {
+        return [:]
+    }
+}
--- LightAlarm/ViewController.swift
+++ LightAlarm/ViewController.swift
@@ -8,10 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ServiceCallProtocol {
 
+    // Constants
+    let testSetUrl = "http://www.schempc.com/lightAlarm/?setHour=07&setMin=35"
+    let baseAlarmUrl = "http://www.schempc.com/lightAlarm/?"
+    let getTimeUrl = "http://www.schempc.com/lightAlarm/?getAlarmTime=true"
+    
+    // UI outlets
+    @IBOutlet weak var dateTimePicker: UIDatePicker!
+    @IBOutlet weak var setAlarmButton: UIButton!
+    
+    var serviceCaller: ServiceCaller {
+        get {
+            return ServiceCaller(invoker: self)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        dateTimePicker.datePickerMode = UIDatePickerMode.Time
+        serviceCaller.makeCall(getTimeUrl)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +37,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func setAlarmTime(sender: UIButton) {
+        let timeFormatter = NSDateFormatter()
+        timeFormatter.timeStyle = NSDateFormatterStyle.ShortStyle
+        
+        let alarmTime = timeFormatter.stringFromDate(dateTimePicker.date)
+        print(alarmTime)
+        let date = dateTimePicker.date
+        
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components([NSCalendarUnit.Hour, NSCalendarUnit.Minute] , fromDate: date)
+        let hour = components.hour
+        let minute = components.minute
+        
+        let setAlarmUrl = "\(baseAlarmUrl)setHour=\(hour)&setMin=\(minute)"
+//        let setAlarmUrl = testSetUrl
+        serviceCaller.makeCall(setAlarmUrl)
+    }
 
 }
 
+extension ViewController {
+    func updateData(data: NSArray) {
+        print("data: \(data)")
+    }
+}
